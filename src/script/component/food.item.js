e06fd6cc55f6cfa5a79fa4982582904e0265d1f9
Muhammad Ubbadah Al Ala <muhammadubbadah@pg.student.pens.ac.id>
2021-09-01 08:31:28 +0700

    design

--- src/script/component/food.item.js
+++ src/script/component/food.item.js
@@ -7,47 +7,25 @@ class FoodItem extends HTMLElement {
 
     render() {
         this.innerHTML = `
-        <body class="font-mono bg-gray-400">
-            <!-- Container -->
-            <div class="container mx-auto">
-                <div class="flex justify-center px-6 my-12">
-                    <!-- Row -->
-                    <div class="w-full xl:w-3/4 lg:w-11/12 flex">
-                        <!-- Col -->
-                        <div class="w-full h-auto hidden bg-gray-400 lg:block lg:w-1/2 bg-cover rounded-2xl" style="background-image: url('${this._food.strMealThumb}')"></div>
-                        <!-- Col -->
-                        <div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-2xl">
-                            <div class="px-8 mb-4 text-center">
-                                <h3 class="pt-4 mb-2 text-3xl font-bold">${this._food.strMeal}</h3>
-                                <p class="mb-4 text-sm text-gray-700">
-                                    <div class="px-4 pt-4 pb-2">
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Area: ${this._food.strArea}</span>
-                                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Category: ${this._food.strCategory}</span>
-                                    </div>
-                                    <div class="mb-6 text-center">
-                                        <a target="_blank" href="${this._food.strYoutube}" class="w-full px-4 py-2 font-bold text-white bg-red-600 rounded-full hover:bg-red-800 focus:outline-none focus:shadow-outline">
-									        Watch on Youtube
-								        </a>
-                                    </div>
-                                </p>
-                            </div>
-                            <div class="px-8 pt-3 pb-8 mb-4 bg-white rounded">
-                                <div class="mb-4">
-                                    <label class="block mb-2 text-sm font-bold text-gray-700">
-                                        How to make it:
-                                    </label>
-                                </div>
-                                <div class="text-center">
-                                    <p class="inline-block text-sm">
-                                        ${this._food.strInstructions}
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+        <div class="max-w-md my-8 mx-auto bg-white rounded-3xl shadow-md md:max-w-2xl">
+            <div class="md:flex">
+                <div class="md:flex-shrink-0">
+                    <img class="h-48 w-full rounded-3xl object-cover md:h-full md:w-48" src="${this._food.strMealThumb}" alt="Man looking at item at a store">
+                </div>
+                <div class="p-8">
+                    <div class="uppercase tracking-wide text-5xl text-yellow-500 font-black">${this._food.strMeal}</div>
+                    <div class="pt-4 pb-2 content-between">
+                        <span class="inline-block bg-yellow-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Area: ${this._food.strArea}</span>
+                        <span class="inline-block bg-yellow-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Category: ${this._food.strCategory}</span>
                     </div>
+                    <div class="m-0">
+                        <span class="inline-block bg-red-100 hover:bg-red-500 hover:text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"><a href="${this._food.strYoutube}" target=”_blank”>Watch on Youtube</a></span>
+                    </div>
+                    <p class="mt-2 text-gray-700 font-bold">How to make:</p>
+                    <p class="mt-2 text-gray-500 max-h-40 overflow-auto">${this._food.strInstructions}</p>
                 </div>
             </div>
-        </body>
+        </div>
         `;
     }
 }
