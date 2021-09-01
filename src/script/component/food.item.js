ac877995ade140a17648df5c4b044eefcf86b587
Muhammad Ubbadah Al Ala <muhammadubbadah@pg.student.pens.ac.id>
2021-09-01 08:42:10 +0700

    padding food item

--- src/script/component/food.item.js
+++ src/script/component/food.item.js
@@ -7,22 +7,24 @@ class FoodItem extends HTMLElement {
 
     render() {
         this.innerHTML = `
-        <div class="max-w-md my-8 mx-auto bg-white rounded-3xl shadow-md md:max-w-2xl">
-            <div class="md:flex">
-                <div class="md:flex-shrink-0">
-                    <img class="h-48 w-full rounded-3xl object-cover md:h-full md:w-48" src="${this._food.strMealThumb}" alt="Man looking at item at a store">
-                </div>
-                <div class="p-8">
-                    <div class="uppercase tracking-wide text-5xl text-yellow-500 font-black">${this._food.strMeal}</div>
-                    <div class="pt-4 pb-2 content-between">
-                        <span class="inline-block bg-yellow-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Area: ${this._food.strArea}</span>
-                        <span class="inline-block bg-yellow-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Category: ${this._food.strCategory}</span>
+        <div class="flex justify-center px-6 my-2">
+            <div class="max-w-md my-8 mx-auto bg-white rounded-3xl shadow-md md:max-w-md">
+                <div class="md:flex">
+                    <div class="md:flex-shrink-0">
+                        <img class="h-48 w-full rounded-3xl object-cover md:h-full md:w-48" src="${this._food.strMealThumb}" alt="Man looking at item at a store">
                     </div>
-                    <div class="m-0">
-                        <span class="inline-block bg-red-100 hover:bg-red-500 hover:text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"><a href="${this._food.strYoutube}" target=”_blank”>Watch on Youtube</a></span>
+                    <div class="p-8">
+                        <div class="uppercase tracking-wide text-5xl text-yellow-500 font-black">${this._food.strMeal}</div>
+                        <div class="pt-4 pb-2 content-between">
+                            <span class="inline-block bg-yellow-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Area: ${this._food.strArea}</span>
+                            <span class="inline-block bg-yellow-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Category: ${this._food.strCategory}</span>
+                        </div>
+                        <div class="m-0">
+                            <span class="inline-block bg-red-100 hover:bg-red-500 hover:text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"><a href="${this._food.strYoutube}" target=”_blank”>Watch on Youtube</a></span>
+                        </div>
+                        <p class="mt-2 text-gray-700 font-bold">How to make:</p>
+                        <p class="mt-2 text-gray-500 max-h-40 overflow-auto">${this._food.strInstructions}</p>
                     </div>
-                    <p class="mt-2 text-gray-700 font-bold">How to make:</p>
-                    <p class="mt-2 text-gray-500 max-h-40 overflow-auto">${this._food.strInstructions}</p>
                 </div>
             </div>
         </div>
