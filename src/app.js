d936da2285b6b86d9a333ad817943371034ee62a
Muhammad Ubbadah Al Ala <muhammadubbadah@pg.student.pens.ac.id>
2021-09-01 13:50:28 +0700

    add scroll component

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import "regenerator-runtime";
 import "tailwindcss/dist/tailwind.min.css"
 import "./styles/style.css";
 
+import "alpinejs/dist/cdn.min.js"
 import main from "./script/view/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
