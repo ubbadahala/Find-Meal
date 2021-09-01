b860c994545d3fb9ccd82b339373d077d152ef96
Muhammad Ubbadah Al Ala <muhammadubbadah@pg.student.pens.ac.id>
2021-09-01 19:22:44 +0700

    add animation

--- src/script/component/food-list.js
+++ src/script/component/food-list.js
@@ -18,11 +18,11 @@ class FoodList extends HTMLElement {
 
     renderError(message) {
         this.innerHTML = `
-        <div class="modal z=5 fixed text-white px-6 py-4 border-0 rounded-2xl bottom-3 left-3 mb-4 bg-red-500">
-            <span class="text-xl inline-block mr-5 align-middle">
+        <div class="modal z=2 animate-bounce fixed text-white px-6 py-4 border-0 rounded-2xl items-center inset-x-1.5 mx-10 lg:mx-96 md:mx-60 sm:mx-40 bottom-20 mb-4 bg-red-500">
+            <span class="text-xl inline-block items-center mr-5 align-middle">
                 <i class="fas fa-bell" />
             </span>
-            <span class="inline-block align-middle mr-8">
+            <span class="inline-block items-center align-middle">
                 <b class="capitalize">Error!</b> ${message}!
             </span>
         </div>
